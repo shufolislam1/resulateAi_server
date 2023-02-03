a564d94f72d7f2b4285a84dfca00084d8739c07e
shufol islam <shufolislam1255@gmail.com>
2023-02-03 20:54:49 +0600

    basic setup full done

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ const port = 5000
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('Running sawarieeeeeeeeeeeeeeeeee')
+})
+
+app.listen(port, () => {
+    console.log('listening', port);
+})
