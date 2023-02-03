a9175daf1ad3ead1144f4a28a5830e02ed65cabc
shufol islam <shufolislam1255@gmail.com>
2023-02-03 20:46:28 +0600

    basic setup half done

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config()
+
+
+const app = express()
+const port = 5000
+
+// middleware
+app.use(cors())
+app.use(express.json())
