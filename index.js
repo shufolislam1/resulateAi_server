22d575a4f36cfdf58b3bc55e6f5eeebd8065f7f9
shufol islam <shufolislam1255@gmail.com>
2023-02-03 22:16:46 +0600

    managestudents route working

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ const port = process.env.PORT;
 
 
 // schema
-const schema = new mongoose.Schema({ name: 'string', price: Number, quantity: Number });
+const schema = new mongoose.Schema({
+    fname: 'string',
+    mname: 'string',
+    lname: 'string',
+    selectClasses: Number,
+    selectDivisions: 'string',
+    rollNumber : Number,
+    adress1: 'string',
+    adress2: 'string',
+    landmark: 'string',
+    city: 'string',
+    pinCode: Number,
+});
 const test = mongoose.model('test', schema);
 
 // middleware
@@ -19,10 +31,26 @@ app.use(express.json())
 // username: BengalSoft, pass: ybeXXUiq6cwuXQwB
 mongoose.set('strictQuery', true);
 mongoose.connect(`mongodb+srv://resulateAi:${process.env.DB_PASS}@resulateai.c5cu7yb.mongodb.net/?retryWrites=true&w=majority`)
-.then(() => {console.log('Database connected successfully')})
-.catch(error => console.log(error))
+    .then(() => { console.log('Database connected successfully') })
+    .catch(error => console.log(error))
 
 
+    
+// routes
+app.post('/addDetails', async (req, res) => { //routes er bodol e app. use kora lagse. route use korle kaj kore na
+    const addnewItem = new test(req.body);
+    const savedItem= await addnewItem.save();
+    res.send(savedItem);
+})
+
+// get the info in table
+app.get('/manageStudents',  async (req,res)=> {
+    const info = await test.find({});
+    res.send(info);
+   // console.log('something');
+})
+
+// testing server is runnig or not
 app.get('/', (req, res) => {
     res.send('Running sawarieeeeeeeeeeeeeeeeee')
 })
@@ -30,6 +58,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('listening', port);
 })
-
-// resulateAi
-// wNyBiDbIAo5Im5tI
