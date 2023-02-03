60bf3425c0866d1509eb0c5782c5af15da43b0db
shufol islam <shufolislam1255@gmail.com>
2023-02-03 21:15:07 +0600

    env setup, database connextion done

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
-
+const mongoose = require('mongoose');
 
 const app = express()
-const port = 5000
+const port = process.env.PORT;
+
+
+// schema
+const schema = new mongoose.Schema({ name: 'string', price: Number, quantity: Number });
+const test = mongoose.model('test', schema);
 
 // middleware
 app.use(cors())
 app.use(express.json())
+
+// connexting mongoose
+// username: BengalSoft, pass: ybeXXUiq6cwuXQwB
+mongoose.set('strictQuery', true);
+mongoose.connect(`mongodb+srv://resulateAi:${process.env.DB_PASS}@resulateai.c5cu7yb.mongodb.net/?retryWrites=true&w=majority`)
+.then(() => {console.log('Database connected successfully')})
+.catch(error => console.log(error))
 
 
 app.get('/', (req, res) => {
@@ -18,3 +30,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('listening', port);
 })
+
+// resulateAi
+// wNyBiDbIAo5Im5tI
